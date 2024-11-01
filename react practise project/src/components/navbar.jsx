5404b8b4aa758ab7c913f8f1ceb762dd67d7b0e6
wahyuanang <wahyuanng@students.amikom.ac.id>
2024-11-01 20:57:41 +0700

    setup project react js

---
+++ react practise project/src/components/navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <div>
+      <h1>Hallo Ges</h1>
+      <ul>
+        <li>Home</li>
+        <li>About me</li>
+        <li>Contat</li>
+        <li>FAQ</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
